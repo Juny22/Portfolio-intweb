fb0ef39ce1a027e78ce840853e8ae6425fe94972
Lang Ya <lang.ya351@gmail.com>
2023-12-07 00:46:56 +0100

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,9 +35,7 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
-        </Navbar.Brand>
+        <img src={logo} className="img-fluid logo" alt="brand" />
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           onClick={() => {
